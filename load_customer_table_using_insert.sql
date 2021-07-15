571a586881ce42cd6b129ed32bb9698a36606a60
gsarkhawas <51230865+gsarkhawas@users.noreply.github.com>
2021-07-15 14:26:47 +0530

    Update load_customer_table_using_insert.sql

--- load_customer_table_using_insert.sql
+++ load_customer_table_using_insert.sql
@@ -1,3 +1,4 @@
+
 insert into table hospital_customers values ('D', 'Alex', '123457', '2010-10-12', '2010-10-13', 'MVD', 'Paul', 'SA', 'USA', '1987-03-06', 'A');
 insert into table hospital_customers values ('D', 'John', '123458', '2010-10-12', '2010-10-13', 'MVD', 'Paul', 'TN', 'IND', '1987-03-06', 'A');
 insert into table hospital_customers values ('D', 'Mathew', '123459', '2010-10-12', '2010-10-13', 'MVD', 'Paul', 'WAS', 'PHIL', '1987-03-06', 'A');
