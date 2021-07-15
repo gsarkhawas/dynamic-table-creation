b410fa5984ec6f85fcea12ae9a633c2ccc04a94d
gsarkhawas <51230865+gsarkhawas@users.noreply.github.com>
2021-07-15 14:25:34 +0530

    Update create_customer_table.sql

--- create_customer_table.sql
+++ create_customer_table.sql
@@ -1,3 +1,4 @@
+
 create table hospital_customers(Customer_Name varchar(255), Customer_Id varchar(18), Customer_Open_Date date,
   Last_Consulted_Date date, Vaccination_Id char(5), Doctor_Name char(255), State char(5),
   Country char(5), DOB date, Is_Active char(1))
